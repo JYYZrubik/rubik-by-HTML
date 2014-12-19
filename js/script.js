3283ddf40010f85a009ed743554ba48111512be0
geno1024 <754097987@qq.com>
2014-12-20 07:45:45 +0800

    JS getElementById finished and fs created

---
+++ js/script.js
@@ -0,0 +1,72 @@
+var ulb=document.getElementById("ulb");
+var ub=document.getElementById("ub");
+var urb=document.getElementById("urb");
+
+var ul=document.getElementById("ul");
+var u=document.getElementById("u");
+var ur=document.getElementById("ur");
+
+var ulf=document.getElementById("ulf");
+var uf=document.getElementById("uf");
+var urf=document.getElementById("urf");
+
+var lub=document.getElementById("lub");
+var lu=document.getElementById("lu");
+var luf=document.getElementById("luf");
+
+var flu=document.getElementById("flu");
+var fu=document.getElementById("fu");
+var fru=document.getElementById("fru");
+
+var ruf=document.getElementById("ruf");
+var ru=document.getElementById("ru");
+var rub=document.getElementById("rub");
+
+var bru=document.getElementById("bru");
+var bu=document.getElementById("bu");
+var blu=document.getElementById("blu");
+
+var lb=document.getElementById("lb");
+var l=document.getElementById("l");
+var lf=document.getElementById("lf");
+
+var fl=document.getElementById("fl");
+var f=document.getElementById("f");
+var fr=document.getElementById("fr");
+
+var rf=document.getElementById("rf");
+var r=document.getElementById("r");
+var rb=document.getElementById("rb");
+
+var br=document.getElementById("br");
+var b=document.getElementById("b");
+var bl=document.getElementById("bl");
+
+var ldb=document.getElementById("ldb");
+var ld=document.getElementById("ld");
+var ldf=document.getElementById("ldf");
+
+var fld=document.getElementById("fld");
+var fd=document.getElementById("fd");
+var frd=document.getElementById("frd");
+
+var rdf=document.getElementById("rdf");
+var rd=document.getElementById("rd");
+var rdb=document.getElementById("rdb");
+
+var brd=document.getElementById("brd");
+var bd=document.getElementById("bd");
+var bld=document.getElementById("bld");
+
+var dlf=document.getElementById("dlf");
+var df=document.getElementById("df");
+var drf=document.getElementById("drf");
+
+var dl=document.getElementById("dl");
+var d=document.getElementById("d");
+var dr=document.getElementById("dr");
+
+var dlb=document.getElementById("dlb");
+var db=document.getElementById("db");
+var drb=document.getElementById("drb");
+
